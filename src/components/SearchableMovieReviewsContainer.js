97f3ff8c1ed01a209942984605be12ca99c73b67
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-03-31 16:31:14 -0500

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,8 +3,8 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+`api-key=${NYT_API_KEY}&query=`;
 
 export default class SearchableMovieReviewsContainer extends Component{
     constructor(){
@@ -30,17 +30,32 @@ export default class SearchableMovieReviewsContainer extends Component{
     render(){
         return(
             <div className="searchable-movie-reviews">
-                <form onSubmit={this.handleSubmission}>
-                    <label>Search Movie Reviews:</label>
-                    <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange}></input>
-                    <input type="submit" />
-                </form>
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="search-input">Search Movie Reviews</label>
+              <input
+                id="search-input"
+                type="text"
+                style={{ width: 300 }}
+                onChange={this.handleSearchInputChange}
+              />
+              <button type="submit">Submit</button>
+            </form>
+            {typeof this.state.reviews === 'object' &&
+              this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
+            <MovieReviews reviews={this.state.reviews} />
+          </div>
+            // <div className="searchable-movie-reviews">
+            //     <form onSubmit={this.handleSubmission}>
+            //         <label>Search Movie Reviews:</label>
+            //         <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange}></input>
+            //         <input type="submit" />
+            //     </form>
 
-                {typeof this.state.reviews === 'object' &&
-                this.state.reviews.length > 0 }
-                <MovieReviews reviews={this.state.reviews} />
+            //     {/* {typeof this.state.reviews === 'object' &&
+            //     this.state.reviews.length > 0 }
+            //     <MovieReviews reviews={this.state.reviews} /> */}
 
-            </div>
+            // </div>
         )
     }
 }
